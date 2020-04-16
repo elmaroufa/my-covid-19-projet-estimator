6381274d70784cbc9978f7192b1fe24b922d6955
Saly Abbo <elmaroufa@gmail.com>
2020-04-16 13:22:24 +0100

    update code and review error by lint

--- src/estimator.js
+++ src/estimator.js
@@ -1,56 +1,50 @@
-// function return timeToElapse in the days format 
+// function return timeToElapse in the days format
 const convertTimeToElaoseToDay = (periodType, timeToElapse) => {
-    
-    if(periodType === 'weeks')
-   {
-       return timeToElapse*7
-   }
-   else if (periodType === 'months')
-    {
-        return timeToElapse * 30;
-    }
-    else {
-        return timeToElapse;
-    }
+  if (periodType === 'weeks') {
+		return timeToElapse * 7
+  }
+  else if (periodType === 'months') {
+    return timeToElapse * 30;
+  }
+	else {
+    return timeToElapse;
+  }
 }
 
 // function calcul power to day using estimate currentlyInfected
-const powerToDays = (days) => {
-   return Math.trunc(days / 3);
-}
+const powerToDays = (days) => Math.trunc(days / 3);
+
 
 // function calcul currentlyInfected
-const calculCurrentlyInfected = (reportedCases, multiple) => {
-   return reportedCases * multiple;
-}
+const calculCurrentlyInfected = (reportedCases, multiple) => reportedCases * multiple;
 
 /* function calcul infection by requested 
 @param currentlyInfected
 @param mypowerToDays
 */
 const calculInfectionByRequestedTime = (currentlyInfected, mypowerToDays) => {
-   return currentlyInfected * Math.pow(2, mypowerToDays);
+	return currentlyInfected * (2 ** mypowerToDays);
 }
 
 // function calcul severe CaseBy requested Time
 const calculSevereCaseByrequestedTime = (infectionByRequestedTime) => {
-   return Math.trunc(infectionByRequestedTime * 0.15);
+	return Math.trunc(infectionByRequestedTime * 0.15);
 }
 
 // function calcul value hospital beds by requested time
 const caclulHospitalBedsByRequestedTime = (hospitalBeds, severeCasesByRequestedTime) => {
-   let value = 0.35 * hospitalBeds;
-   return Math.trunc(value - severeCasesByRequestedTime);
+	let value = 0.35 * hospitalBeds;
+  return Math.trunc(value - severeCasesByRequestedTime);
 }
 
 // function calcul casesForICUByRequestedTime
 const calculcasesForICUByRequestedTime = (infectionsByRequestedTime) => {
-   return Math.trunc( 0.05 * infectionsByRequestedTime);
+	return Math.trunc( 0.05 * infectionsByRequestedTime);
 }
 
 // function calcul casesForVentilatorsByRequestedTime
 const calculcasesForVentilatorsByRequestedTime = (infectionsByRequestedTime) => {
-   return Math.trunc(0.02 * infectionsByRequestedTime);
+	return Math.trunc(0.02 * infectionsByRequestedTime);
 }
 
 /* function to calcul dollarsInFlight
@@ -59,56 +53,61 @@ const calculcasesForVentilatorsByRequestedTime = (infectionsByRequestedTime) => 
 * @param avgDailyIncomeInUSD
 * @param avgDailyIncomePopulation
 */
-const calculDollarsInFlight = (infectionByRequestedTime, periodeDay, avgDailyIncomeInUSD, avgDailyIncomePopulation) => {
-   return Math.trunc(infectionByRequestedTime * periodeDay * avgDailyIncomePopulation * avgDailyIncomeInUSD);
+const calculDollarsInFlight = (infectionByRequestedTime, 
+	periodeDay, avgDailyIncomeInUSD, avgDailyIncomePopulation) => {
+	return Math.trunc(infectionByRequestedTime * periodeDay * avgDailyIncomePopulation * avgDailyIncomeInUSD);
 }
 
 const covid19ImpactEstimator = (data) => {
-    const input = data;
-    let daysCacul = convertTimeToElaoseToDay(data.periodType, data.timeToElapse);
-    let mypowerToDays = powerToDays(daysCacul);
+	
+  const daysCacul = convertTimeToElaoseToDay(data.periodType, data.timeToElapse);
+  const mypowerToDays = powerToDays(daysCacul);
 
-    /**
-     * variable end Ip = impact  and variable end Si = SevereImpact
-     * declaration des variables et calcul des attributs de impact et severeimpact
-     */
-    let valCurrentlyInfectedIp = calculCurrentlyInfected(data.reportedCases, 10);
-    let valCurrentlyInfectedSi = calculCurrentlyInfected(data.reportedCases, 50);
-    let valInfectionByRequestedTimeIp = calculInfectionByRequestedTime(valCurrentlyInfectedIp, mypowerToDays);
-    let valInfectionByRequestedTimeSi = calculInfectionByRequestedTime(valCurrentlyInfectedSi, mypowerToDays);
-    let valSevereCaseByrequestedTimeIp = calculSevereCaseByrequestedTime(valInfectionByRequestedTimeIp);
-    let valSevereCaseByrequestedTimeSi = calculSevereCaseByrequestedTime(valInfectionByRequestedTimeSi);
-    let valHospitalBedsByRequestedTimeIp = caclulHospitalBedsByRequestedTime(data.totalHospitalBeds, valSevereCaseByrequestedTimeIp);
-    let valHospitalBedsByRequestedTimeSi = caclulHospitalBedsByRequestedTime(data.totalHospitalBeds, valSevereCaseByrequestedTimeSi);
-    let valCasesForICUByRequestedTimeIp = calculcasesForICUByRequestedTime(valInfectionByRequestedTimeIp);
-    let valCasesForICUByRequestedTimeSi = calculcasesForICUByRequestedTime(valInfectionByRequestedTimeSi);
-    let valcasesForVentilatorsByRequestedTimeIp = calculcasesForVentilatorsByRequestedTime(valInfectionByRequestedTimeIp);
-    let valcasesForVentilatorsByRequestedTimeSi = calculcasesForVentilatorsByRequestedTime(valInfectionByRequestedTimeSi);
-    let valdollarsInFlightIp = calculDollarsInFlight(valInfectionByRequestedTimeIp, daysCacul, data.region.avgDailyIncomeInUSD, data.region.avgDailyIncomePopulation);
-    let valdollarsInFlightSi = calculDollarsInFlight(valInfectionByRequestedTimeSi, daysCacul, data.region.avgDailyIncomeInUSD, data.region.avgDailyIncomePopulation)
-    return {
-        data: data,
-        estimate: {
-        impact: {
-            currentlyInfected: valCurrentlyInfectedIp,
-            infectionByRequestedTime: valInfectionByRequestedTimeIp,
-            severeCasesByRequestedTime: valSevereCaseByrequestedTimeIp,
-            hospitalBedsByRequestedTime: valHospitalBedsByRequestedTimeIp,
-            casesForICUByRequestedTime: valCasesForICUByRequestedTimeIp,
-            casesForVentilatorsByRequestedTime: valcasesForVentilatorsByRequestedTimeIp,
-            dollarsInFlight: valdollarsInFlightIp
-        },
-        severeImpact: {
-            currentlyInfected: valCurrentlyInfectedSi,
-            infectionByRequestedTime: valInfectionByRequestedTimeSi,
-            severeCasesByRequestedTime: valSevereCaseByrequestedTimeSi,
-            hospitalBedsByRequestedTime: valHospitalBedsByRequestedTimeSi,
-            casesForICUByRequestedTime: valCasesForICUByRequestedTimeSi,
-            casesForVentilatorsByRequestedTime: valcasesForVentilatorsByRequestedTimeSi,
-            dollarsInFlight: valdollarsInFlightSi
-        }
-     }
-    }
+  /**
+   * variable end Ip = impact  and variable end Si = SevereImpact
+   * declaration des variables et calcul des attributs de impact et severeimpact
+  */
+  const valCurrentlyInfectedIp = calculCurrentlyInfected(data.reportedCases, 10);
+  const valCurrentlyInfectedSi = calculCurrentlyInfected(data.reportedCases, 50);
+  const valInfectionByRequestedTimeIp = calculInfectionByRequestedTime(valCurrentlyInfectedIp, mypowerToDays);
+  const valInfectionByRequestedTimeSi = calculInfectionByRequestedTime(valCurrentlyInfectedSi, mypowerToDays);
+  const valSevereCaseByrequestedTimeIp = calculSevereCaseByrequestedTime(valInfectionByRequestedTimeIp);
+  const valSevereCaseByrequestedTimeSi = calculSevereCaseByrequestedTime(valInfectionByRequestedTimeSi);
+	const valHospitalBedsByRequestedTimeIp = caclulHospitalBedsByRequestedTime(data.totalHospitalBeds, 
+		valSevereCaseByrequestedTimeIp);
+	const valHospitalBedsByRequestedTimeSi = caclulHospitalBedsByRequestedTime(data.totalHospitalBeds, 
+		valSevereCaseByrequestedTimeSi);
+  const valCasesForICUByRequestedTimeIp = calculcasesForICUByRequestedTime(valInfectionByRequestedTimeIp);
+  const valCasesForICUByRequestedTimeSi = calculcasesForICUByRequestedTime(valInfectionByRequestedTimeSi);
+  const valcasesForVentilatorsByRequestedTimeIp = calculcasesForVentilatorsByRequestedTime(valInfectionByRequestedTimeIp);
+  const valcasesForVentilatorsByRequestedTimeSi = calculcasesForVentilatorsByRequestedTime(valInfectionByRequestedTimeSi);
+	const valdollarsInFlightIp = calculDollarsInFlight(valInfectionByRequestedTimeIp, daysCacul, 
+		data.region.avgDailyIncomeInUSD, data.region.avgDailyIncomePopulation);
+	const valdollarsInFlightSi = calculDollarsInFlight(valInfectionByRequestedTimeSi, daysCacul, 
+		data.region.avgDailyIncomeInUSD, data.region.avgDailyIncomePopulation);
+  return {
+			data: data,
+    	estimate: {
+      impact: {
+      	currentlyInfected: valCurrentlyInfectedIp,
+        infectionByRequestedTime: valInfectionByRequestedTimeIp,
+        severeCasesByRequestedTime: valSevereCaseByrequestedTimeIp,
+        hospitalBedsByRequestedTime: valHospitalBedsByRequestedTimeIp,
+        casesForICUByRequestedTime: valCasesForICUByRequestedTimeIp,
+        casesForVentilatorsByRequestedTime: valcasesForVentilatorsByRequestedTimeIp,
+        dollarsInFlight: valdollarsInFlightIp
+        	},
+      severeImpact: {
+      	currentlyInfected: valCurrentlyInfectedSi,
+        infectionByRequestedTime: valInfectionByRequestedTimeSi,
+        severeCasesByRequestedTime: valSevereCaseByrequestedTimeSi,
+        hospitalBedsByRequestedTime: valHospitalBedsByRequestedTimeSi,
+        casesForICUByRequestedTime: valCasesForICUByRequestedTimeSi,
+        casesForVentilatorsByRequestedTime: valcasesForVentilatorsByRequestedTimeSi,
+        dollarsInFlight: valdollarsInFlightSi
+					}
+				}
+  };
 };
 
 export default covid19ImpactEstimator;
